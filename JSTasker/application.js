74845469ecc512b70085d3552f98380d94bee546
Tony Huynh <tony@29fx.com>
2010-10-31 11:13:48 -0400

    add trash link, check/uncheck all, plus/minus links

--- JSTasker/application.js
+++ JSTasker/application.js
@@ -1,26 +1,75 @@
 var JSTasker = {
   update_counter : function() {
-    var task_count = $('#tasks ul li').not('li.completed').size();
+    var task_count = $('#tasks ul#pending li').size();
     $('span#task_counter').text("(" + task_count + ")");    
   },
-  sort_tasks : function() {
-    var completed = $('#tasks ul li.completed');
-    completed.detach();
-    $('#tasks ul').append(completed);
-  },
-  update_page : function() {
-    this.update_counter();
-    this.sort_tasks();
-  },
-  check_all : function() {
-    $('#tasks ul li').each(function() {
-      $(this).addClass('completed');
+  move_to_completed : function() {
+    var completed = $('#tasks ul#pending li.completed');
+    completed.slideUp('slow', function() {
+      completed.detach();
+      $('#tasks ul#completed').append(completed);
+      completed.slideDown();      
     });
   },
+  move_to_pending : function() {
+    var pending = $('#tasks ul#completed li').not('.completed');
+    pending.slideUp('slow', function() {
+      pending.detach();
+      $('#tasks ul#pending').append(pending);
+      pending.slideDown();      
+    });
+  },
+  sort_tasks : function() {
+    this.move_to_completed();
+    this.move_to_pending();
+  },
+  update_page : function() {
+    this.sort_tasks();
+    this.update_counter();
+  },
+  check_all : function() {
+    $('#tasks ul#pending li').click();
+    
+    // NOTE: stop using each when you don't have to!
+    // $('#tasks ul#pending li').each(function() {
+    //   $(this).addClass('completed');
+    // });
+  },
   uncheck_all : function() {
-    $('#tasks ul li.completed').each(function() {
-      $(this).removeClass('completed');
-    });    
+    $('#tasks ul#completed li').click();
+    
+    // $('#tasks ul li').each(function() {
+    //   $(this).removeClass('completed');
+    // });    
+  },
+  trash_icon : function() {
+    // create trash icon with event handler
+    var trash = $("<img src='icons/bin.png'/ class='trash'>")
+    trash.click(function (event) {
+      var item_to_trash = $(this).parent();
+      item_to_trash.fadeOut('slow', function() {
+        $(this).remove();
+        // since we stop propogation need to update count explicitly
+        JSTasker.update_counter();
+      });
+      event.stopPropagation();
+    });
+    return trash;
+  },
+  move_up : function (event) {
+    // find the li
+    // find the li before this one
+    // use 'before' to insert this one before the previous 
+    var mover = $(this).parent();
+    var target = mover.prev();
+    target.before(mover);
+    event.stopPropagation();
+  },
+  move_down : function (event) {
+    var mover = $(this).parent();
+    var target = mover.next();
+    target.after(mover);
+    event.stopPropagation();
   }
 }
 
@@ -30,32 +79,44 @@ $(document).ready(function () {
     event.preventDefault();
     
     var task_text = $('input#task_text').val();
-    var list_item = $("<li>" + task_text  + "</li>");
+    var list_item = $("<li>" + task_text + "</li>");
+
     list_item.click( function () {
       $(this).toggleClass('completed');
       JSTasker.update_page();
     });
+    list_item.append(JSTasker.trash_icon());
+
+    var minus = $("<span class='minus'>-</span>");
+    minus.click(JSTasker.move_down);
+    list_item.append(minus);
+    var plus = $("<span class='plus'>+</span>");
+    plus.click(JSTasker.move_up);
+    list_item.append(plus);
     
-    $('#tasks ul').append(list_item);
+    $('#tasks ul#pending').append(list_item);
     list_item.effect('highlight', 'slow');
     $('input#task_text').val(null);
     
     JSTasker.update_page();
   });
   
-  $('a#check_all').click( function(event) {
-    event.preventDefault();
-    JSTasker.check_all();
-  });
+  // $('a.trash').live('click', function(event) {
+  //   event.preventDefault();
+  //   console.log($(this).parent());
+  // });
+  
+  // NOTE: can't use not check_all() b/c it'll be evaluated upon load
+  // passing in function, not result of function
+  $('span#check_all').click(JSTasker.check_all); 
 
-  $('a#uncheck_all').click( function(event) {
-    event.preventDefault();
-    JSTasker.uncheck_all();
-  });
+  $('span#uncheck_all').click(JSTasker.uncheck_all);
     
   $('input#task_text').focus();
   
   $('#tasks h2').append("<span id='task_counter'></span>");
-  
-  // alert($("input[name='add_button']").text());
+
 });
+
+
+
